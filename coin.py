15a24c40b95f0ae08be7746f585adbe0a9a5ea96
Syedusman91 <116784818+Syedusman91@users.noreply.github.com>
2022-11-04 11:16:08 +0500

    Add files via upload

---
+++ coin.py
@@ -0,0 +1,49 @@
+while(True):
+#     amount=int(input("Enter Remaining Money: "))
+#     if amount >= 25:
+#         twenty_five=amount//25
+#         ten=(amount % 25) // 10
+#         five=((amount % 25 ) % 10) // 5
+#         one = (((amount % 25) % 10) % 5) //1
+#         print("No of 25 Coins :",twenty_five)
+#         print("No of 10 Coins :",ten)
+#         print("No of  5 Coins :",five)
+#         print("No of  1 Coins :",one)
+#         print("Total No of coins :", twenty_five + ten + five + one)
+#         print()
+
+#     elif amount < 25 and amount >= 10:
+#         twenty_five=amount//25
+#         ten=(amount % 25) // 10
+#         five=((amount % 25 ) % 10) // 5
+#         one = (((amount % 25) % 10) % 5) //1
+#         print("No of 25 Coins :",twenty_five)
+#         print("No of 10 Coins :",ten)
+#         print("No of  5 Coins :",five)
+#         print("No of  1 Coins :",one)
+#         print("Total No of coins :", twenty_five + ten + five + one,"\n")
+
+#     elif amount < 10 and amount >=5:
+#         twenty_five=amount//25
+#         ten=(amount % 25) // 10
+#         five=((amount % 25 ) % 10) // 5
+#         one = (((amount % 25) % 10) % 5) //1
+#         print("No of 25 Coins :",twenty_five)
+#         print("No of 10 Coins :",ten)
+#         print("No of  5 Coins :",five)
+#         print("No of  1 Coins :",one)
+#         print("Total No of coins :", twenty_five + ten + five + one,"\n")
+
+#     elif amount < 5 and amount >= 1 :
+#         twenty_five=amount//25
+#         ten=(amount % 25) // 10
+#         five=((amount % 25 ) % 10) // 5
+#         one = (((amount % 25) % 10) % 5) //1
+#         print("No of 25 Coins :",twenty_five)
+#         print("No of 10 Coins :",ten)
+#         print("No of  5 Coins :",five)
+#         print("No of  1 Coins :",one)
+#         print("Total No of coins :", twenty_five + ten + five + one,"\n")
+
+#     elif amount == 0:
+#         print("You don't have to return any coin\n")
